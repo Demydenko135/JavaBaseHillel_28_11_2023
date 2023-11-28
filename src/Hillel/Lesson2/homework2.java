4a6d9b202c1728be56e8dc665af0f44016e564b8
Demydenko135 <fostersiker1@gmail.com>
2023-11-28 12:44:41 +0200

    Add lessons

---
+++ src/Hillel/Lesson2/homework2.java
@@ -0,0 +1,36 @@
+package Hillel.Lesson2;
+
+public class homework2 {
+    public static void main(String[] args) {
+        double A =  40.730610;
+        double B = -73.935242;
+        System.out.println("New York City, NY, USA");
+        System.out.println("Latitude : "  + A );
+        System.out.println("Longitude : " + B);
+    }
+}
+
+//public class homework2 {
+//    public static void main(String[] args){
+//        byte  A = 40;
+//        char  B  = '\u00B0';
+//        byte C = 43;
+//        char D = '\'';
+//        double E = 50.196;
+//        byte extra = 0;
+//        char F = '\"';
+//        char G  = 'N';
+//        byte H = 73;
+//        char I  = '\u00B0';
+//        byte K = 56;
+//        char L = '\'';
+//        double M = 6.8712;
+//        char N = '\"';
+//        char O = 'W';
+//
+//
+//        System.out.println("New York City, NY, USA");
+//        System.out.println("DMS Lat - " + A + B + " " + C + D + " " + E + extra + F + " " + G);
+//        System.out.println("DMS Long - " + H + I + " " + K + L + " " + M + N + " " + O);
+//    }
+//}
