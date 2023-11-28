43af36da6a7295515d928e487b8d17ac064e678c
Demydenko135 <fostersiker1@gmail.com>
2023-11-28 12:45:47 +0200

    Updates 12-45

--- src/Hillel/lesson3/Homework3.java
+++ src/Hillel/lesson3/Homework3.java
@@ -2,6 +2,6 @@ package Hillel.lesson3;
 
 public class Homework3 {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git update");
     }
 }
