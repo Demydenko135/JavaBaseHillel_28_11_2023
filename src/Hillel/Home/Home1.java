3a2744a215660f0f54296359a25044eea2b829b1
Demydenko135 <fostersiker1@gmail.com>
2023-11-28 18:36:21 +0200

    Just for testing

---
+++ src/Hillel/Home/Home1.java
@@ -0,0 +1,30 @@
+package Hillel.Home;
+import java.util.Random;
+public class Home1 {
+    public static void main(String[] args) {
+        int x = new Random().nextInt(100);
+        int y = new Random().nextInt(100);
+        System.out.println("Два рандомних числа - " + x + " " + y);
+        System.out.println("Cума = " + (x + y));
+        System.out.println("Різниця = " + (x - y) );
+        System.out.println("Добуток = " + (x * y));
+        if (x > y) {
+            System.out.println( "first greater");
+        }
+            else {
+                System.out.println( "second greater" );
+            }
+        if (y > x) {
+            System.out.println( "first greater");
+        }
+        else {
+            System.out.println("second greater");
+        }
+        if (x == y) {
+            System.out.println("Equals");
+        }
+            else {
+            System.out.println("Not Equals");
+        }
+    }
+}
