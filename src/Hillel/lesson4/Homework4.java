055fe136910b96c4a06e0fac6ea2f344b659bce1
Demydenko135 <fostersiker1@gmail.com>
2023-11-30 22:00:43 +0200

    Drafts, just for saving results

---
+++ src/Hillel/lesson4/Homework4.java
@@ -0,0 +1,23 @@
+package Hillel.lesson4;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        int li = 860;
+     double mi = 860f * 1.5f;
+     int a = li * 13;
+     int b = li * 24;
+     int c = li * 46;
+    int LiAvg = a + b + c;
+        System.out.println("Загальний показник атаки динатії Мінь = " + LiAvg);
+     double a1 = mi * 9;
+     double b1 = mi * 35;
+     double c1 = mi * 12;
+     double MiAvg = a1 + b1 + c1;
+        System.out.println("Загальний показник атаки динатії Мінь = " + (int) MiAvg);
+//        System.out.println(a + b+ c);
+//        System.out.println(a1 + b1+ c1);
+//
+//
+//        System.out.println(li + " " + (int) mi);
+    }
+}
