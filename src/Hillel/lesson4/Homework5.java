77539d6e062c57c04c7b2967465b2a449fb5959c
Demydenko135 <fostersiker1@gmail.com>
2023-12-01 09:25:07 +0200

    1 december, PROD:)

---
+++ src/Hillel/lesson4/Homework5.java
@@ -0,0 +1,14 @@
+package Hillel.lesson4;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 8;
+        int volume = a * b * c;
+        int length = 4 * (a + b + c);
+        System.out.println ("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Сумарна довжина - " + length);
+
+    }
+}
