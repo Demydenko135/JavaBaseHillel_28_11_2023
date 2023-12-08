b9307d87dda0ebf7c39af40590981167b4825fd3
Demydenko135 <fostersiker1@gmail.com>
2023-12-08 12:38:31 +0200

    practise

---
+++ src/Hillel/Home/for_while_dowhile.java
@@ -0,0 +1,13 @@
+package Hillel.Home;
+
+public class for_while_dowhile {
+    public static void main(String[] args) {
+        for (int i = 0; i >  10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
