dc5f1fc8e38b66d235468776fdfc882e7877c395
Demydenko135 <fostersiker1@gmail.com>
2023-12-11 22:59:05 +0200

    lesson6

---
+++ src/Hillel/Home/lesson6/lesson6.java
@@ -0,0 +1,13 @@
+package Hillel.Home.lesson6;
+
+public class lesson6 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <=20 ; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+            
+        }
+    }
+}
