91a593216f75083aa3ca9cc9cbb317c6bdd89df9
Demydenko135 <fostersiker1@gmail.com>
2023-12-12 09:48:20 +0200

    lesson6

---
+++ src/Hillel/Home/lesson6/homework7.java
@@ -0,0 +1,39 @@
+package Hillel.Home.lesson6;
+
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+
+public class homework7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int random = (int) (Math.random() * 11);
+
+        int value = 3;
+        int counter = 0;
+        System.out.println("Введіть своє число :");
+
+        while (value > counter) {
+            System.out.println("Ваше число");
+            int yourvalue = scanner.nextInt();
+            counter++;
+            if (yourvalue == random){
+                System.out.println("Ви перемогли");
+                break;
+            }else {
+                System.out.println("Спробуйте ще раз ");
+            }
+
+            }
+        if (value == counter) {
+            System.out.println("Ви використали всі спроби,випадкове число було = " + random);
+        }
+
+
+
+
+
+
+
+    }
+
+}
