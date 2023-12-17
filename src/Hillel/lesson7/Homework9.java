704d22514f1737ddd30195a9cccc22993cfc5f43
Demydenko135 <fostersiker1@gmail.com>
2023-12-17 20:38:25 +0200

    Homework 9 first draft(2)

--- src/Hillel/lesson7/Homework9.java
+++ src/Hillel/lesson7/Homework9.java
@@ -6,29 +6,25 @@ public class Homework9 {
     public static void main(String[] args) {
         int[] teamOne = new int[25];
         int[] teamTwo = new int[25];
-        int counter = 0;
+
+
 
         for (int i = 0; i < teamOne.length; i++) {
             teamOne[i] = (int) (Math.random() * (40 - 18 + 1) + 18);
             teamTwo[i] = (int) (Math.random() * (40 - 18 + 1) + 18);
-//
-//   Не сильно зрозумів чому саме при такій формулі воно видає правильний діапазон.
-           counter ++;
 
-//
-            }
-        System.out.print(Arrays.toString(teamOne) );
-        System.out.println(counter);
+
+        }
+        System.out.print ( Arrays.toString(teamOne) );
         System.out.println();
         System.out.print(Arrays.toString(teamTwo) );
-        System.out.println(counter);
-            counter++;
 
         }
 
     }
 
 
+//   Не сильно зрозумів чому саме при такій формулі воно видає правильний діапазон.
 
 //    Є дві команди регбі з 25 гравців різного віку у кожній.
 //
