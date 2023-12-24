36fce4f4eb09851744761622d6b49eacba146f50
Demydenko135 <fostersiker1@gmail.com>
2023-12-24 12:15:29 +0200

    Done.

--- src/Hillel/Home/lesson8/Homework10.java
+++ src/Hillel/Home/lesson8/Homework10.java
@@ -1,8 +1,6 @@
 package Hillel.Home.lesson8;
 
 import java.util.Arrays;
-//import java.util.SortedMap;
-
 
 public class Homework10 {
     public static void main(String[] args) {
@@ -10,18 +8,23 @@ public class Homework10 {
         int[] SecondValue = new int[7];
         int counter = 0;
 
-        for (int i = 0; i < FirstValue.length - 1; i++) {
+        for (int i = 0; i < FirstValue.length; i++) {
             FirstValue[i] = (int) (Math.random() * 10);
             SecondValue[i] = (int) (Math.random() * 10);
-            if (FirstValue[i] == SecondValue[i])
-                counter++;
         }
         Arrays.sort(FirstValue);
         Arrays.sort(SecondValue);
 
-        System.out.println(Arrays.toString(FirstValue ));
+        System.out.println(Arrays.toString(FirstValue));
         System.out.println(Arrays.toString(SecondValue));
+        for (int i = 0; i < FirstValue.length; i++) {
+            if (FirstValue[i] == SecondValue[i]) {
+                counter++;
+            }
+        }
         System.out.println("Кількість збігів :  " + counter);
-
     }
 }
+
+
+
