fd2f2e190db90abe1374bfc496345c59030d4e6c
Demydenko135 <fostersiker1@gmail.com>
2023-12-27 15:06:16 +0200

    ready for prod

--- src/Hillel/Lesson9.java
+++ src/Hillel/Lesson9.java
@@ -5,9 +5,26 @@ import java.util.Scanner;
 
 public class Lesson9 {
     public static void main(String[] args) {
+        System.out.println("Ввведіть M значення : ");
         Scanner scanner = new Scanner(System.in);
-        int M = scanner.nextInt();
-        int N = scanner.nextInt();
+        int M = 0;
+        int N = 0;
+        if (scanner.hasNextInt()) {
+            M = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Ви ввели не правильні дані, перезапустіть будь ласка програму !!");
+            System.exit(0);
+        }
+        System.out.println("Введіть N значення : ");
+        if (scanner.hasNextInt()) {
+            N = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Ви ввели не правильні дані, перезапустіть будь ласка програму !!");
+            System.exit(0);
+        }
+
 
         int[][] Pasha1 = new int[N][M];
         int[][] Pasha2 = new int[M][N];
@@ -18,15 +35,15 @@ public class Lesson9 {
                 Pasha2[j][i] = Pasha1[i][j];
             }
         }
+        System.out.println("Результат : ");
 
         for (int i = 0; i < Pasha1.length; i++) {
             System.out.println(Arrays.toString(Pasha1[i]));
         }
+        System.out.println("Транспортована матриця : ");
         for (int i = 0; i < Pasha2.length; i++) {
             System.out.println(Arrays.toString(Pasha2[i]));
 
         }
     }
 }
-
-//    Транспонуйте матрицю M x N. Розміри M та N задаються з консолі. Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.
