60a897c42ef7c860c0f1e96b65190e668cd113a5
Demydenko135 <fostersiker1@gmail.com>
2023-12-28 12:12:37 +0200

    ready for prod 3

--- src/Hillel/Homework11.java
+++ src/Hillel/Homework11.java
@@ -12,38 +12,47 @@ public class Homework11 {
         if (scanner.hasNextInt()) {
             M = scanner.nextInt();
             scanner.nextLine();
+            if (M <= 0) {
+                System.out.println("Введіть додатне значення для M. Перезапустіть програму !!");
+                System.exit(0);
+            }
         } else {
-            System.out.println("Ви ввели не правильні дані, перезапустіть будь ласка програму !!");
-            System.exit(0);
-        }
-        System.out.println("Введіть N значення : ");
-        if (scanner.hasNextInt()) {
-            N = scanner.nextInt();
-            scanner.nextLine();
-        } else {
-            System.out.println("Ви ввели не правильні дані, перезапустіть будь ласка програму !!");
-            System.exit(0);
-        }
+                System.out.println("Ви ввели не правильні дані, перезапустіть будь ласка програму !!");
+                System.exit(0);
+            }
+            System.out.println("Введіть N значення : ");
+            if (scanner.hasNextInt()) {
+                N = scanner.nextInt();
+                scanner.nextLine();
+                if (N <= 0) {
+                    System.out.println("Введіть додатне значення для M. Перезапустіть програму !!");
+                    System.exit(0);
+                }
+                } else {
+                    System.out.println("Ви ввели не правильні дані, перезапустіть будь ласка програму !!");
+                    System.exit(0);
+                }
 
 
-        int[][] Pasha1 = new int[N][M];
-        int[][] Pasha2 = new int[M][N];
+                int[][] Pasha1 = new int[N][M];
+                int[][] Pasha2 = new int[M][N];
 
-        for (int i = 0; i < Pasha1.length; i++) {
-            for (int j = 0; j < Pasha1[i].length; j++) {
-                Pasha1[i][j] = (int) (Math.random() * 11);
-                Pasha2[j][i] = Pasha1[i][j];
+                for (int i = 0; i < Pasha1.length; i++) {
+                    for (int j = 0; j < Pasha1[i].length; j++) {
+                        Pasha1[i][j] = (int) (Math.random() * 11);
+                        Pasha2[j][i] = Pasha1[i][j];
+                    }
+                }
+                System.out.println("Результат : ");
+
+                for (int i = 0; i < Pasha1.length; i++) {
+                    System.out.println(Arrays.toString(Pasha1[i]));
+                }
+                System.out.println("Транспортована матриця : ");
+                for (int i = 0; i < Pasha2.length; i++) {
+                    System.out.println(Arrays.toString(Pasha2[i]));
+
+                }
             }
         }
-        System.out.println("Результат : ");
 
-        for (int i = 0; i < Pasha1.length; i++) {
-            System.out.println(Arrays.toString(Pasha1[i]));
-        }
-        System.out.println("Транспортована матриця : ");
-        for (int i = 0; i < Pasha2.length; i++) {
-            System.out.println(Arrays.toString(Pasha2[i]));
-
-        }
-    }
-}
