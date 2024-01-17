0e8273fe2de837ffabb55e66060767bc5ddaddfe
Demydenko135 <fostersiker1@gmail.com>
2024-01-17 21:12:23 +0200

    Homework 12 4 draft

--- src/Hillel/Home/lesson11/BurgerMain.java
+++ src/Hillel/Home/lesson11/BurgerMain.java
@@ -4,75 +4,44 @@ public class BurgerMain {
 
     public static void main(String[] args) {
 
-        burger BurgerStandart = new burger();
-
-        burger BurgerVegan = new burger(true, true, true, true);
-
-        burger BurgerWithExtraMeet = new burger(true, true, true, true, true);
+        Burger burgerStandard = new Burger();
+        Burger burgerVegan = new Burger(true, true, true, true);
+        Burger burgerDoubleMeat = new Burger(true, true, true, true, true);
 
     }
-
 }
 
-class burger {
+class Burger {
 
     private boolean bun;
-
     private boolean meat;
-
     private boolean cheese;
-
     private boolean veggies;
-
     private boolean mayo;
 
-    public burger() {
-
+    public Burger() {
         this.bun = true;
-
         this.meat = true;
-
         this.cheese = true;
-
         this.veggies = true;
-
         this.mayo = true;
-
-        System.out.println("Звичайний бургер : булочка, м'ясо, сир, зелень, майонез. ");
-
+        System.out.println("Звичайний бургер: булочка, м'ясо, сир, зелень, майонез.");
     }
 
-    public burger(boolean bun, boolean meat, boolean cheese, boolean veggies) {
-
-        this.bun = true;
-
-        this.meat = true;
-
-        this.cheese = true;
-
-        this.veggies = true;
-
-//        this.mayo = true;
-
+    public Burger(boolean bun, boolean meat, boolean cheese, boolean veggies) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.veggies = veggies;
         System.out.println("Дієтичний бургер (без майонезу): булочка, м'ясо, сир, зелень.");
-
     }
 
-    public burger(boolean bun, boolean meat, boolean cheese, boolean veggies, boolean mayo) {
-
-        this.bun = true;
-
-        this.meat = true;
-
-        this.cheese = true;
-
-        this.veggies = true;
-
-        this.mayo = true;
-
-
-        System.out.println("Бургер з подвійним м'ясом: булочка, подвійне мʼясо, сир, зелень, майонез.");
-
+    public Burger(boolean bun, boolean meat, boolean cheese, boolean veggies, boolean mayo) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.veggies = veggies;
+        this.mayo = mayo;
+        System.out.println("Бургер з подвійним м'ясом: булочка, подвійне м'ясо, сир, зелень, майонез.");
     }
 }
-
