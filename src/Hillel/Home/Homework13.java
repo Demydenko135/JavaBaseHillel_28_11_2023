dd9477eb93b0db67297b7b541dde1245863e815c
Demydenko135 <fostersiker1@gmail.com>
2024-01-24 20:35:04 +0200

    Homework 15 2 draft

---
+++ src/Hillel/Home/Homework13.java
@@ -0,0 +1,40 @@
+package Hillel.Home;
+public class Homework13 {
+    static class MusicStyles {
+        public void playMusic() {
+            System.out.println("Playing generic music");
+        }
+    }
+
+    static class PopMusic extends MusicStyles {
+        @Override
+        public void playMusic() {
+            System.out.println("Playing pop music");
+        }
+    }
+
+    static class RockMusic extends MusicStyles {
+        public void playMusic() {
+            System.out.println("Playing rock music");
+        }
+    }
+
+    static class ClassicMusic extends MusicStyles {
+        @Override
+        public void playMusic() {
+            System.out.println("Playing classical music");
+        }
+    }
+
+    public static void main(String[] args) {
+        PopMusic popMusic = new PopMusic();
+        RockMusic rockMusic = new RockMusic();
+        ClassicMusic classicMusic = new ClassicMusic();
+
+        MusicStyles[] musicBands = {popMusic, rockMusic, classicMusic};
+
+        for (MusicStyles band : musicBands) {
+            band.playMusic();
+        }
+    }
+}
